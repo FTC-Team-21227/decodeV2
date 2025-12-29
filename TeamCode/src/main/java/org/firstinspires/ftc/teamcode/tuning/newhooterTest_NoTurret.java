cf2fba5db6456d56d530df3f424afcc7191a5dde
FTC-Team-21227 <teamlegendftc@gmail.com>
2025-12-29 15:24:30 -0800

    sloth fix, pedro tune, pedro auto move, dual flywheel, shooter calc verify with pedrogeometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/newhooterTest_NoTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/newhooterTest_NoTurret.java
@@ -4,13 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel_Old;
+import org.firstinspires.ftc.teamcode.subsystems.Hood;
 
-@TeleOp(name = "Shooter Test No Turret")
-public class shooterTest_NoTurret extends LinearOpMode {
+@TeleOp(name = "New Shooter Test No Turret")
+public class newhooterTest_NoTurret extends LinearOpMode {
 
-    Flywheel_Old flywheel;
-    Servo hood;
+    Flywheel flywheel;
+    Hood hood;
     // Editable in dashboard, distance and height difference from robot to goal
     public static double distance;
     public static double heightDiff;
@@ -22,30 +24,39 @@ public class shooterTest_NoTurret extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 //        telemetry = new MultipleTelemetry (FtcDashboard.getInstance().getTelemetry());
-        flywheel = new Flywheel_Old(hardwareMap);
+        flywheel = new Flywheel(hardwareMap);
 //        hood = hardwareMap.get(Servo.class, "hood");
         //        initialization();
         waitForStart();
         double hoodPos = 0.5;
         flywheelVelocity = 0;
         while (opModeIsActive()) {
-//            if (gamepad1.left_bumper){ // Move hood down
-//                hoodPos += 0.001;
-//                hood.setPosition(hoodPos);
-//            }
-//            if (gamepad1.right_bumper){ // Move hood up
-//                hoodPos -= 0.001;
-//                hood.setPosition(hoodPos);
-//            }
+            if (gamepad1.left_bumper){ // Move hood down
+                hoodPos += 0.001;
+            }
+            if (gamepad1.right_bumper){ // Move hood up
+                hoodPos -= 0.001;
+            }
             if (gamepad1.dpad_up){
                 flywheelVelocity += 5;
             }
             if (gamepad1.dpad_down){
                 flywheelVelocity -= 5;
             }
-            flywheel.spinTo(flywheelVelocity);
+            if (gamepad1.b){
+                flywheelVelocity = 0;
+                flywheel.FLYWHEEL_MASTER.setPower(0);
+                flywheel.FLYWHEEL2.setPower(0);
+            }
+            else flywheel.spinTo(flywheelVelocity);
+            hood.HOOD.setPosition(hoodPos);
             telemetry.addData("Flywheel Target Vel", flywheelVelocity);
             telemetry.addData("Flywheel Current Vel", flywheel.getVel());
+            telemetry.addData("Hood target pos", hoodPos);
+            telemetry.addData("Hood curr angle", hood.getAngle());
+            telemetry.addData("Hood pos", hood.HOOD.getPosition());
+            telemetry.addData("outside range", hood.HOOD.commandedOutsideRange());
+            telemetry.update();
 //            telemetry.addData("hood pos", hood.getPosition());
             telemetry.update();
 
