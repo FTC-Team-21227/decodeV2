6c1855b3e72f0495dc14beefae0598a975e5843c
gragao314 <oafish1982@gmail.com>
2026-01-31 16:29:16 -0800

    pedro auto constants tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,20 +16,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .forwardZeroPowerAcceleration(-40.356022741184866)
-            .lateralZeroPowerAcceleration(-50.625262888087825)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.25, 0, 0.01, 0))
-            .headingPIDFCoefficients(new PIDFCoefficients(1.5, 0, 0.01, 0))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.04,0.0,0.00001,0.6,0.0))
-            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.01,0.0,0.000005,0.6,0.0))
-            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.02, 0, 0.001, 0))
-            .centripetalScaling(0.0005)
-            .mass(8); // kg mass of robot
+            .mass(11.65732) // kg mass of robot
+            .forwardZeroPowerAcceleration(-35.8478891252221)
+            .lateralZeroPowerAcceleration(-75.16567708403743);
+//            .translationalPIDFCoefficients(new PIDFCoefficients(0.25, 0, 0.01, 0))
+//            .headingPIDFCoefficients(new PIDFCoefficients(1.5, 0, 0.01, 0))
+//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.04,0.0,0.00001,0.6,0.0))
+//            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.01,0.0,0.000005,0.6,0.0))
+//            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.02, 0, 0.001, 0))
+//            .centripetalScaling(0.0005)
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
-            .xVelocity(60.89125601700912)
-            .yVelocity(52.443046029158474)
+            .xVelocity(68.49260417307455)
+            .yVelocity(54.68980443759229)
 //            .useVoltageCompensation(true)
             .rightFrontMotorName("W_FR") // motor names, keep consistent on configs
             .rightRearMotorName("W_BR")
